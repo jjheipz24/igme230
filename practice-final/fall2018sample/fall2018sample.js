14d0f940d77fadeabb87d4314f2800cefbb4fe01
Jin Jin Heipler (RIT Student) <jxh9922@ad.rit.edu>
2018-12-04 12:51:33 -0500

    practice final

--- practice-final/fall2018sample/fall2018sample.js
+++ practice-final/fall2018sample/fall2018sample.js
@@ -1,1 +1,17 @@
 /* Don't use <script> tags in a linked js file! */
+$(".submenu").hide();
+$("#menu1").click(function () {
+    $("#sub1").slideToggle("slow");
+});
+$("#menu2").click(function () {
+    $("#sub2").slideToggle("slow");
+});
+
+let content = ("content1.txt"); //sets default
+$("#choose-content").val(content); //changes menu to default
+$("#content").load(content); //loads default
+
+$("#choose-content").change(function () {
+    content = $(this).val();
+    $("#content").load(content);
+});
